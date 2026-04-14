93a7dc6e7f44dcca9aea9ac51f59fb3a60c4c8c5
Luca Krohn <lv-krohn@bremershei.de>
2026-04-14 08:22:13 +0200

    Add groupfilter example

---
+++ examples/can_filter.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file can_filter.cpp
+ * @brief Example of how to use the buildGroupFilter function in the BIP26_CAN
+ * 
+ * temp
+ * 
+ * @author Luca Krohn
+ */
+#include <Arduino.h>
+#include <BIP26_CAN.h>
+
+#define groupLsb 20
+#define groupMsb 23
+
+
+BIP26_CAN can;
+uint8_t groupID;
+void buildGroupID();
+
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+
+    buildGroupID();
+
+    uint8_t groupfilter = 0x5; // example group filter, only accept messages from group 0x5
+
+    while(!can.begin(groupID, 18, 19, TWAI_MODE_NORMAL, can.buildGroupFilter(groupfilter))){
+        delay(100);
+    }
+}
+
+/**
+ * Just listens for messages and prints their content.
+ * only messages from group 0x5 will be received due to the filter config used in the begin function.
+ */
+void loop(){
+    Serial.print("Group ");
+    Serial.print(groupID);
+    Serial.println(" is listening...");
+    twai_message_t msg;
+    while(can.receive(msg)){
+        Serial.print("Message from Group ");
+        Serial.print(msg.identifier >> 4 & 0xF, HEX);
+        Serial.print(" received with id: ");
+        Serial.println(msg.identifier, HEX);
+        for(int i = 0; i < msg.data_length_code; i++){
+            Serial.print(msg.data[i], HEX);
+        }
+        Serial.println();
+    }
+}
+
+/**
+ * Reads the state of the pins defined by groupLsb and groupMsb and builds the groupID for the CAN messages.
+ */
+void buildGroupID(){
+  groupID = 0;
+  for (int i = groupLsb; i <= groupMsb; i++){
+    pinMode(i, INPUT);
+    groupID |= ((digitalRead(i) & 1) << (i-groupLsb));
+  }
+}
+
